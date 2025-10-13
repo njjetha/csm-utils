45176e040d2b069836a2647694f2133fcea176b6
Parikshit Thakur <parithak@qti.qualcomm.com>
2025-10-13 14:50:33 +0530

    feat(csm-utils): add csm-event-handler.sh to manage CSM services via uevents
    Introduced csm-event-handler.sh script to start or restart CSM services
    based on uevents. This enhances automated service management triggered by system uevents.
    
    Signed-off-by: Parikshit thakur <parithak@qti.qualcomm.com>

---
+++ csm-event-handler.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#******************************************************************************
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#******************************************************************************/
+
+DEVICE="$1"
+echo "csm-event: starting script csm-event-handler" > /dev/kmsg
+
+if [[ "$DEVICE" == *"SAHARA"* ]]; then
+    echo "csm-event: $DEVICE: starting csm-nbdkit service for SAHARA" > /dev/kmsg
+    systemd-run --no-block systemctl restart csm-nbdkit@"$DEVICE".service &
+    echo "csm-event: $DEVICE: csm-nbdkit service completed with status:$?" > /dev/kmsg
+else
+    echo "csm-event: $DEVICE: starting csm-configure-ip service for CSM_CTRL" > /dev/kmsg
+    systemd-run --no-block systemctl restart csm-configure-ip@"$DEVICE".service &
+    echo "csm-event: $DEVICE: csm-configure-ip service completed with status:$?" > /dev/kmsg
+fi
